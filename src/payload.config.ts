00570264e5859728bd0a81b986accf40bcbbb752
Ahmed Elbashaar <ahmed.bashaar@outlook.com>
2025-09-05 23:05:05 +0300

    Rename environmental variable VERCEL_BLOB_ACCESS_TOKEN -> BLOB_READ_WRITE_TOKEN

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -41,7 +41,7 @@ export default buildConfig({
       collections: {
         media: true,
       },
-      token: process.env.VERCEL_BLOB_TOKEN || '',
+      token: process.env.BLOB_READ_WRITE_TOKEN || '',
     }),
   ],
 })
